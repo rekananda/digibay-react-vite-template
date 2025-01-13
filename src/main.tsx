39c96cf8305cddea9b2bfbe5da1922d18b14e4ba
Reka <reka.reka.rn@gmail.com>
2025-01-13 23:48:12 +0700

    setup mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { MantineProvider } from '@mantine/core';
 import './index.css'
+// mantine css
+import '@mantine/core/styles.css'
+import '@mantine/carousel/styles.css'
+import '@mantine/charts/styles.css'
+import '@mantine/dates/styles.css'
+import '@mantine/dropzone/styles.css'
+import '@mantine/nprogress/styles.css'
+import '@mantine/tiptap/styles.css'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
